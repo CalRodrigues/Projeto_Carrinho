0fa780bc5de4939c799d56949ae6c2324949d174
Carol Rodrigues <carolrodriguesdev@gmail.com>
2022-12-21 22:53:42 +0000

    finalizando funcoes do carrinho

--- main.js
+++ main.js
@@ -11,4 +11,28 @@ function adicionarItem(item) {
 
   quantidade.innerHTML = qtd[item];
   total.innerHTML = valorTotal[item].toFixed(2);
+  valorCompra();
+};
+
+function removerItem(item) {
+  if (qtd[item] > 0) {
+    qtd[item] -= 1;
+    var quantidade = document.getElementById("quantidade" + item);
+    var total = document.getElementById("total" + item);
+    quantidade.innerHTML = qtd[item];
+    valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
+    total.innerHTML = valorTotal[item].toFixed(2);
+    valorCompra();
+  }
+};
+
+function valorCompra() {
+  var valorTotalCompra = document.getElementById("valorTotalCompra");
+  var valor = 0;
+
+  for(let i = 0; i < valorTotal.length; i++) {
+    valor += valorTotal[i];
+  }
+
+  valorTotalCompra.innerHTML = valor.toFixed(2);
 };
